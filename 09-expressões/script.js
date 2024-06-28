35e56ed387307c604773a97115f00593ca932a12
Vitor dos Santos Gonçalves <vitinho6.6@hotmail.com>
2024-06-28 19:38:26 -0300

    :books: Expressões

---
+++ 09-expressões/script.js
@@ -0,0 +1,14 @@
+const grupoA = 100;
+const grupoB = 300;
+
+const vencedor = grupoA > grupoB ? "Grupo A Ganhou" : "Grupo B Ganhou";
+
+const grupoAvenceu = grupoA > 50 && "Grupo A Vencedor";
+console.log(grupoAvenceu);
+console.log(vencedor);
+
+const areaQuadrado = (l) => l * l;
+
+const numeros = [2, 3, 4, 5, 6];
+const total = numeros.filter((numeros) => numeros > 4);
+console.log(total);
